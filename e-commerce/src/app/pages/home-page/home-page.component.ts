fd84cb95a9459da63f291307009daebea6dbac01
suhaib al jayyousi <siktiranimos@outlook.com>
2025-03-03 22:41:32 +0300

    new file:   e-commerce/public/assets/images/banner-bg.png 	new file:   e-commerce/public/assets/images/call-icon.png  	new file:   e-commerce/public/assets/images/computer-img.png  	new file:   e-commerce/public/assets/images/dress-shirt-img.png  	new file:   e-commerce/public/assets/images/flag-france.png  	new file:   e-commerce/public/assets/images/flag-uk.png  	new file:   e-commerce/public/assets/images/footer-logo.png  	new file:   e-commerce/public/assets/images/jhumka-img.png  	new file:   e-commerce/public/assets/images/kangan-img.png  	new file:   e-commerce/public/assets/images/laptop-img.png  	new file:   e-commerce/public/assets/images/logo.png  	new file:   e-commerce/public/assets/images/mail-icon.png  	new file:   e-commerce/public/assets/images/map-icon.png  	new file:   e-commerce/public/assets/images/mobile-img.png  	new file:   e-commerce/public/assets/images/neklesh-img.png  	new file:   e-commerce/public/assets/images/search-icon.png  	new file:   e-commerce/public/assets/images/toggle-icon.png  	new file:   e-commerce/public/assets/images/tshirt-img.png  	new file:   e-commerce/public/assets/images/user-icon.png  	new file:   e-commerce/public/assets/images/women-clothes-img.png  	modified:   e-commerce/src/app/app.component.html  	modified:   e-commerce/src/app/app.routes.ts  	modified:   e-commerce/src/app/component/header/header.component.html  	modified:   e-commerce/src/app/pages/home-page/home-page.component.html  	modified:   e-commerce/src/app/pages/home-page/home-page.component.ts

--- e-commerce/src/app/pages/home-page/home-page.component.ts
+++ e-commerce/src/app/pages/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HeaderComponent } from "../../component/header/header.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
