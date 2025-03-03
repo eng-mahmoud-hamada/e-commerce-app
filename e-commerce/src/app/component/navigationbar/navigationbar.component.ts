f0c85345cb83704b3996998150be689dd8165941
mahmoud-300 <80002645+eng-mahmoud-hamada@users.noreply.github.com>
2025-03-03 22:05:53 +0300

    modified:   e-commerce/src/app/app.component.html 	new file:   e-commerce/src/app/component/footer/footer.component.html 	new file:   e-commerce/src/app/component/footer/footer.component.scss 	new file:   e-commerce/src/app/component/footer/footer.component.spec.ts 	new file:   e-commerce/src/app/component/footer/footer.component.ts 	new file:   e-commerce/src/app/component/header/header.component.html 	new file:   e-commerce/src/app/component/header/header.component.scss 	new file:   e-commerce/src/app/component/header/header.component.spec.ts 	new file:   e-commerce/src/app/component/header/header.component.ts 	new file:   e-commerce/src/app/component/item-card/item-card.component.html 	new file:   e-commerce/src/app/component/item-card/item-card.component.scss 	new file:   e-commerce/src/app/component/item-card/item-card.component.spec.ts 	new file:   e-commerce/src/app/component/item-card/item-card.component.ts 	new file:   e-commerce/src/app/component/navigationbar/navigationbar.component.html 	new file:   e-commerce/src/app/component/navigationbar/navigationbar.component.scss 	new file:   e-commerce/src/app/component/navigationbar/navigationbar.component.spec.ts 	new file:   e-commerce/src/app/component/navigationbar/navigationbar.component.ts 	new file:   e-commerce/src/app/component/sidebar/sidebar.component.html 	new file:   e-commerce/src/app/component/sidebar/sidebar.component.scss 	new file:   e-commerce/src/app/component/sidebar/sidebar.component.spec.ts 	new file:   e-commerce/src/app/component/sidebar/sidebar.component.ts 	new file:   e-commerce/src/app/pages/home-page/home-page.component.html 	new file:   e-commerce/src/app/pages/home-page/home-page.component.scss 	new file:   e-commerce/src/app/pages/home-page/home-page.component.spec.ts 	new file:   e-commerce/src/app/pages/home-page/home-page.component.ts

---
+++ e-commerce/src/app/component/navigationbar/navigationbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigationbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navigationbar.component.html',
+  styleUrl: './navigationbar.component.scss'
+})
+export class NavigationbarComponent {
+
+}
